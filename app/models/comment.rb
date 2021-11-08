fd556d4a1fa7a5ea027558e76f4ef1b292988a1e
Sylvestre <pasytchangwa@gmail.com>
2021-11-08 15:37:57 +0200

    Add content in comment.rb model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :users
+  belongs_to :post, counter_cache: :comments_counter
+
+  def update_comment_counter(value)
+    post.update('comments_counter', value)
+  end
 end
