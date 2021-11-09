737538f0e1e6975c2fa035ae145ba840ae767670
Sylvestre <pasytchangwa@gmail.com>
2021-11-09 21:55:45 +0200

    Update db

--- db/migrate/20211104090919_create_users.rb
+++ db/migrate/20211104090919_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :Bio
       t.string :photo
-      t.integer :post_counter
+      t.integer :PostsCounter
 
       t.timestamps
     end
