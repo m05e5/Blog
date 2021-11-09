9f816c215868327ebc8814140b471e67bb7a9379
Sylvestre <pasytchangwa@gmail.com>
2021-11-09 19:23:39 +0200

    Create test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  user = User.create(name: "user ##{i}", bio: "A bio.")
+  3.times do |j|
+    post = Post.create(title: "Awesome Post", text: "Lorem Ipsum Dolor Amet Dolore", user: user)
+    5.times do |j|
+      Comment.create(test: "Comment ##{j}", post: post, user: user)
+    end
+  end
+end
