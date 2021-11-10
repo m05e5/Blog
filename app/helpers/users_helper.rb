fa15ac8a6ef8b8560cacd47fcc2e7938cc2e9ca7
Sylvestre <pasytchangwa@gmail.com>
2021-11-10 13:48:01 +0200

    Modify seed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def get_photo(user)
-    user.photo.present? ? user.photo : "https://eu.ui-avatars.com/api/?name=Sylvestre&background=random"
+    user.photo.present? ? user.photo : 'https://eu.ui-avatars.com/api/?name=Sylvestre&background=random'
   end
 end
