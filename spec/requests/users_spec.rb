76824fdc28b57d40465e3aaae086aed37fa4cdf1
Sylvestre <pasytchangwa@gmail.com>
2021-11-10 14:24:24 +0200

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template(:index)
     end
 
-    # it 'should have correct placeholder text' do
-    #   expect(response.body).to include('Here is a list of users')
-    # end
+    it 'should have correct placeholder text' do
+      expect(response.body).to include('Here is a list of users')
+    end
   end
 
   describe 'GET #show' do
@@ -28,8 +28,8 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template(:show)
     end
 
-    # it 'should have correct placeholder text' do
-    #   expect(response.body).to include('Here is a given user bio and posts')
-    # end
+    it 'should have correct placeholder text' do
+      expect(response.body).to include('Here is a given user bio and posts')
+    end
   end
 end
