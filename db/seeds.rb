8afdaa68ae6aa59b065e2fd399bdc81437801432
Sylvestre <pasytchangwa@gmail.com>
2021-11-10 17:40:16 +0200

    Fix views as required by TSE

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 5.times do |i|
-  user = User.create(name: "user ##{i}", bio: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages. It encourages and facilitates the use of web standards such as JSON or XML for data transfer and HTML, CSS and JavaScript for user interfacing. In addition to MVC, Rails emphasizes the")
-  3.times do |j|
+  user = User.create(name: "John Doe##{i}", bio: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages. It encourages and facilitates the use of web standards such as JSON or XML for data transfer and HTML, CSS and JavaScript for user interfacing. In addition to MVC, Rails emphasizes the")
+  5.times do |j|
     post = Post.create(title: "A beautiful day", text: "Lorem Ipsum Dolor Amet Dolores ", user: user)
     5.times do |j|
       Comment.create(text: "Comment ##{j}", post: post, user: user)
