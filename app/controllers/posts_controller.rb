873a12e40f1de40617326a2e4c43aac782a2a97d
Sylvestre <pasytchangwa@gmail.com>
2021-11-11 10:48:20 +0200

    Modify Post controller file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,30 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    @comments = @post.comments.all
+    @comments = @post.comments.all.order('created_at')
+    @liked = @post.liked? current_user.id
+  end
+
+  def new; end
+
+  def create
+    post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          redirect_to user_post_path(post.user.id, post.id), notice: 'Post added successfully'
+        else
+          flash.now[:alert] = 'Post could not be added'
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:data).permit(:title, :text)
   end
 end
