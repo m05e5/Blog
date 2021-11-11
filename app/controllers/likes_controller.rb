76e096d5c3d97833c76fe45bad990c6ad2ce8d84
Sylvestre <pasytchangwa@gmail.com>
2021-11-11 12:48:16 +0200

    Create partials and modify files

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    comment = post.likess.new(user: current_user)
+    like = post.likes.new(user: current_user)
 
     respond_to do |format|
       format.html do
         if like.save
-          redirect_to user_post_path(post.user.id, post.id), notice: 'Liked successfully'
+          redirect_to user_post_path(post.user.id, post.id), notice: 'Liked'
         else
           redirect_to user_post_path(post.user.id, post.id), alert: 'Could not be liked'
         end
