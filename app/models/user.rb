d20be26d11572a541cdb512e28f72b70e095afe5
Sylvestre <pasytchangwa@gmail.com>
2021-11-11 21:20:59 +0200

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
+
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -7,3 +11,4 @@ class User < ApplicationRecord
     posts.order('created_at Desc').limit(3)
   end
 end
+
