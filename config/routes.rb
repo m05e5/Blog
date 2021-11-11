552d1396545174d4c388eed076ac58b7b1b8e535
Sylvestre <pasytchangwa@gmail.com>
2021-11-11 22:29:55 +0200

    Modify partial files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
-  resources :posts, only: [:new, :create] do
+  resources :posts, only: [:new] do
     resources :comments, only: [:create]
     resources :likes, only: [:create]
   end
 
-  root 'users#index'
+  post '/posts/new', to: 'posts#create' as: 'create_post'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,10 @@
   user = User.create(name: "John Doe##{i}", bio: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages. It encourages and facilitates the use of web standards such as JSON or XML for data transfer and HTML, CSS and JavaScript for user interfacing. In addition to MVC, Rails emphasizes the")
   5.times do |j|
     post = Post.create(title: "A beautiful day", text: "Lorem Ipsum Dolor Amet Dolores ", user: user)
-    5.times do |j|
-      Comment.create(text: "Comment ##{j}", post: post, user: user)
+    5.times do |k|
+      Comment.create(text: "Comment ##{k}", post: post, user: user)
     end
+    post.likes.create(user: user)
   end
 end
 
